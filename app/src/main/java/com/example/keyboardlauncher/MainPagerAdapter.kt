63d0c60fcb0f9efacf0bb50fc90e4cdc40b8ad7c
chdhmbr <chdhmbr@gmail.com>
2025-12-26 19:49:34 +0530

    Create MainPagerAdapter.kt

---
+++ app/src/main/java/com/example/keyboardlauncher/MainPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.keyboardlauncher
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainPagerAdapter(activity: FragmentActivity) :
+    FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> AppsFragment()
+            1 -> FocusFragment()
+            2 -> NotesFragment()
+            else -> AppsFragment()
+        }
+    }
+}
